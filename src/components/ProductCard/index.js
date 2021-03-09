4efb670d5e8bef6aa893235e1a976f543c3db4ba
Vitor Bisterso dos Santos <vitorbisterso@gmail.com>
2021-03-09 15:46:40 -0300

    Change input in ProductCard

--- src/components/ProductCard/index.js
+++ src/components/ProductCard/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 import convertPriceToBrSyntax from '../../utils'
@@ -19,23 +19,40 @@ const renderButton = isAdminPage => {
   )
 }
 
-const ProductCard = ({ title, price, image, isAdminPage }) => (
-  <div className="productCard-container">
-    <img src={image} alt="product" />
-    <div className="productCard-content">
-      <p className="productCard-title">{title}</p>
-      <div className="productCard-bottom">
-        <p>{convertPriceToBrSyntax(price)}</p>
-        <div className="productCard-units">
-          <p>x</p>
-          <input type="number" />
-          <p>un.</p>
+const ProductCard = ({ title, price, image, isAdminPage }) => {
+  const [units, setUnits] = useState(1)
+
+  const handleOnChangeUnit = e => {
+    const { value } = e.target
+
+    if (value >= 0 && value <= 99 && value.length <= 2) {
+      setUnits(value)
+    }
+  }
+
+  return (
+    <div className="productCard-container">
+      <img src={image} alt="product" />
+      <div className="productCard-content">
+        <p className="productCard-title">{title}</p>
+        <div className="productCard-bottom">
+          <p>{convertPriceToBrSyntax(price)}</p>
+          <div className="productCard-units">
+            <p>x</p>
+            <input
+              type="text"
+              pattern="[0-9][0-9]"
+              value={units}
+              onChange={handleOnChangeUnit}
+            />
+            <p>un.</p>
+          </div>
+          {renderButton(isAdminPage)}
         </div>
-        {renderButton(isAdminPage)}
       </div>
     </div>
-  </div>
-)
+  )
+}
 
 ProductCard.propTypes = {
   title: PropTypes.string.isRequired,
